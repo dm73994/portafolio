cd66555014d6be604c6421853e5403a0f4e2971f
dm73994 <dm31284@gmail.com>
2025-12-14 15:56:44 -0500

    Switch from HashRouter to BrowserRouter in App component

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import AppRouter from './router'
 
 const App = () => {
     return (
-        <HashRouter>
+        <BrowserRouter>
             <AppRouter />
-        </HashRouter>
+        </BrowserRouter>
     )
 }
 
