e627de2cc578175fb9eb3a93a4814107b6718b02
dm73994 <dm31284@gmail.com>
2025-12-14 15:42:26 -0500

    Add new images for skills section and enhance Presentation and Skills components with updated content and styles

--- src/pages/Home/Presentation.jsx
+++ src/pages/Home/Presentation.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 const Presentation = () => {
     return (
         <>
-            <div className='grid-2'>
-                <div className='foto' />
+            <div className="grid-2">
+                <div className="foto" />
 
-                <div className='introduction grid-item-center'>
+                <div className="introduction grid-item-center">
                     <h1>BIENVENIDO</h1>
-                    <p>Soy David Mesa, estudiante de ingeniería de sistemas en la universidad del Cauca, actualmente curso septimo semestre de la carrera</p>
+                    <p>
+                        Me llamo Carlos David Mesa, próximamente ingeniero de
+                        sistemas. A lo largo de mi carrera he trabajado en
+                        diversos proyectos que me han permitido crecer técnica y
+                        profesionalmente.
+                        <br />
+                        <br />
+                        Me gusta leer, jugar ajedrez, parchís y fútbol. La
+                        programación me entretiene tanto que no siento ni un día
+                        de trabajo: "Elige un trabajo que te guste y no tendrás
+                        que trabajar ni un día de tu vida."
+                    </p>
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default Presentation
+export default Presentation;
--- src/pages/skills/index.jsx
+++ src/pages/skills/index.jsx
@@ -1,38 +1,127 @@
-import React, { useEffect, useLayoutEffect, useState } from 'react'
-import Divider from '../components/Divider'
-import MainLayout from '../views/MainLayout'
+import { useLayoutEffect, useState } from 'react';
+import Divider from '../components/Divider';
+import MainLayout from '../views/MainLayout';
 
 const Skills = () => {
-    const [show, setShow] = useState('')
+    const [show, setShow] = useState('');
+
+    const skills = [
+        {
+            name: 'React',
+            time: '4',
+            knoledge: 5,
+            image: 'react.svg',
+        },
+        {
+            name: 'Spring Boot',
+            time: '2',
+            knoledge: 4,
+            image: 'spring-boot.webp',
+        },
+        {
+            name: 'Java',
+            time: '3',
+            knoledge: 4,
+            image: 'java.webp',
+        },
+        {
+            name: 'PostgreSQL, SQLServer, MySQL',
+            time: '1',
+            knoledge: 2,
+            image: 'db.webp',
+        },
+        {
+            name: 'Typescript',
+            time: '2',
+            knoledge: 4,
+            image: 'ts.webp',
+        },
+        {
+            name: 'React Native',
+            time: '1',
+            knoledge: 3,
+            image: 'react.svg',
+        },
+        {
+            name: 'Firebase',
+            time: '1',
+            knoledge: 3,
+            image: 'firebase.webp',
+        },
+        {
+            name: 'GCP',
+            time: '1',
+            knoledge: 2,
+            image: 'gcp.gif',
+        },
+    ];
+
+    const learning = [
+        {
+            name: 'Angular20',
+            image: 'angular.webp',
+        },
+        {
+            name: 'AWS',
+            image: 'aws.webp',
+        },
+        {
+            name: 'Docker',
+            image: 'docker.webp',
+        },
+    ];
 
     useLayoutEffect(() => {
         setTimeout(() => {
             setShow('show');
         }, 10);
-    }, [])
+    }, []);
 
     return (
         <MainLayout>
-            <section className='container'>
-                <h2 style={{ textAlign: 'center' }}>Competencias & habilidades</h2>
-                <article className='skills'>
-                    <img src="images/react.svg" alt="React" className={`skillImg ${show}`} />
-                    <img src="images/html.svg" alt="HTML" className={`skillImg ${show}`} />
-                    <img src="images/css.svg" alt="CSS" className={`skillImg ${show}`} />
-                    <img src="images/git.svg" alt="git" className={`skillImg ${show}`} />
-                    <img src="images/js.svg" alt="javascript" className={`skillImg ${show}`} />
+            <section className="container">
+                <h2 style={{ textAlign: 'center' }}>
+                    Competencias & habilidades
+                </h2>
+                <article className="skills">
+                    {skills.map((skill) => (
+                        <div key={skill.name} className={`skillCard ${show}`}>
+                            <div className="circle-map-node">
+                                <img
+                                    src={`images/${skill.image}`}
+                                    alt={skill.name}
+                                    className="circle-map-image"
+
+                                />
+                            </div>
+                            <h3>{skill.name}</h3>
+                            <p>Experiencia en años: {skill.time}</p>
+                            <p>{'⭐'.repeat(skill.knoledge)}</p>
+                        </div>
+                    ))}
                 </article>
 
                 <Divider />
 
                 <h2 style={{ textAlign: 'center' }}>Aprendiendo...</h2>
-                <article className='skills'>
-                    <img src="images/node.svg" alt="nodeJS" className={`skillImg ${show}`} />
-                    <img src="images/express.svg" alt="express" className={`skillImg ${show}`} />
+                <article className="skills">
+                    {learning.map((skill) => (
+                        <div key={skill.name} className={`skillCard ${show}`}>
+                            <div className="circle-map-node">
+                                <img
+                                    src={`images/${skill.image}`}
+                                    alt={skill.name}
+                                    className="circle-map-image"
+
+                                />
+                            </div>
+                            <h3>{skill.name}</h3>
+                        </div>
+                    ))}
                 </article>
             </section>
         </MainLayout>
-    )
-}
+    );
+};
 
-export default Skills
+export default Skills;
