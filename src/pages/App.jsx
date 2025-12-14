7b83aa52e96a4d151de1398a72b0b7ed706fb779
dm73994 <dm31284@gmail.com>
2025-12-14 15:53:02 -0500

    Update package.json homepage URL, add license to gh-pages dependency, and switch from BrowserRouter to HashRouter in App component

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import AppRouter from './router'
 
 const App = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <AppRouter />
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
